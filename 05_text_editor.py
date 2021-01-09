c5d8696c7e85842686b699e245e78706280c200f
alermol <ermol-2012@yandex.ru>
2021-01-09 23:16:48 +0300

    function realisation

--- 05_text_editor.py
+++ 05_text_editor.py
@@ -11,6 +11,9 @@ string = (
     'two characters I am'
 )
 
-string = [w[0] + ''.join(shuffle_list(list(w[1:-1]))) + w[-1]
-          if len(w) > 2 else w for w in string.split()]
-print(string)
+def shuffle_text(s: str) -> str:
+    s = [w[0] + ''.join(shuffle_list(list(w[1:-1]))) + w[-1]
+         if len(w) > 2 else w for w in s.split()]
+    return ' '.join(s)
+
+print(shuffle_text(string))
