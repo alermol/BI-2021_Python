78fa619247526ad49ada92c47d0aca853fbe6882
alermol <ermol-2012@yandex.ru>
2021-02-18 16:33:51 +0300

    fix translator

--- 02_translator.py
+++ 02_translator.py
@@ -7,4 +7,4 @@ from Bio.Seq import Seq
 def translate(fasta, codon_table='Standard'):
     records_list = SeqIO.index(fasta, 'fasta')
     for record in records_list.values():
-        yield record.translate(table=codon_table)
+        yield record.translate(table=codon_table).seq
